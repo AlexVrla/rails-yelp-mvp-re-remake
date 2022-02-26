3f56954ef181d08f10aaa5254c684432abfe8a70
Alexandre Vrla <alexandre.vrla@gmail.com>
2022-02-26 18:40:35 +0100

    passes tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "generating restaurants..."
+
+10.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+
+puts "generating reviews..."
+
+10.times do
+  review = Review.create!(restaurant_id: rand(1..10), rating: rand(0..5), content: Faker::Restaurant.review)
+end
